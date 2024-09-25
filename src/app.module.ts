6913ae1a7d36d208a39010b5ff3f9cc5ad386273
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2024-09-25 17:53:13 +0100

    Installed dotenv and added my environmental variables, also updated main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
+import { User } from './user/user.entity';
+import { Product } from './product/product.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'password',
-      database: 'nestjs_crud',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      host: process.env.DB_HOST || 'localhost',
+      port: +process.env.DB_PORT || 3306,
+      username: process.env.DB_USERNAME || 'root',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_DATABASE || 'test',
+      entities: [Product, User],
       synchronize: true,
     }),
     UserModule,
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import rateLimit from 'express-rate-limit';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);

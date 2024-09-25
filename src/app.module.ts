e335e818ad56675232d02c94019ddaacdc42508d
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2024-09-25 15:29:06 +0100

    Generate the User and Product modules, services, and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+// import { ProductModule } from './product/product.module';
+// import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'nestjs_crud',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Do not use this in production
+    }),
+    UserModule,
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

c7f23831be18669aff045a3af0b4d1b0e5fd78ce
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2024-09-25 15:31:39 +0100

    Generated product modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-// import { ProductModule } from './product/product.module';
-// import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [

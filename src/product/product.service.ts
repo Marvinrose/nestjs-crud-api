006d4836df655a87039ee7c61cb3af7e0a631034
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2024-09-25 15:43:39 +0100

    Implemented UserService and ProductService

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from './product.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
+  ) {}
+
+  create(product: Partial<Product>) {
+    return this.productRepository.save(product);
+  }
+
+  findAll() {
+    return this.productRepository.find({ relations: ['user'] });
+  }
+
+  findOne(id: string) {
+    return this.productRepository.findOne({
+      where: { id },
+      relations: ['user'],
+    });
+  }
+
+  update(id: string, product: Partial<Product>) {
+    return this.productRepository.update(id, product);
+  }
+
+  remove(id: string) {
+    return this.productRepository.delete(id);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  create(user: Partial<User>) {
+    return this.userRepository.save(user);
+  }
+
+  findAll() {
+    return this.userRepository.find({ relations: ['products'] });
+  }
+
+  findOne(id: string) {
+    return this.userRepository.findOne({
+      where: { id },
+      relations: ['products'],
+    });
+  }
+
+  update(id: string, user: Partial<User>) {
+    return this.userRepository.update(id, user);
+  }
+
+  remove(id: string) {
+    return this.userRepository.delete(id);
+  }
+}
